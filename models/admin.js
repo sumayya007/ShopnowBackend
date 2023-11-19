9b6fd13ca17b44260c3babbcd65d20d365552e42
sumayya <sumayya.kareem6@gmail.com>
2023-11-19 16:50:01 +0530

    updated

--- models/admin.js
+++ models/admin.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv/config");
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect("mongodb+srv://shopnow:V7NIS1V1RCAppVPL@cluster0.3nral.mongodb.net/ShopNow", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
--- models/order.js
+++ models/order.js
@@ -2,7 +2,7 @@ import cartItemData from "./cartitem";
 const mongoose=require("mongoose");
 require("dotenv/config");
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect("mongodb+srv://shopnow:V7NIS1V1RCAppVPL@cluster0.3nral.mongodb.net/ShopNow", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
