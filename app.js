afddd9938d7b10bf3e4a602fc1a898ae4501b184
sumayya <sumayya.kareem6@gmail.com>
2023-11-19 15:37:06 +0530

    updated cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const Grid=require('gridfs-stream');
 const PORT = process.env.PORT || 3000;
 const app = new express();
 app.use(cors({
-  origin: 'http://example.com', // use your actual domain name (or localhost), using * is not recommended
+  origin: 'https://shopnow-bsu7.onrender.com/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true
