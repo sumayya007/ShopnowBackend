542d4d847416fd535776f117f18a36baeb6ce0bf
sumayya <sumayya.kareem6@gmail.com>
2023-11-24 13:59:59 +0530

    added

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -224,7 +224,7 @@ router.post("/login", (req, res) => {
       tags:req.body.product.tags,
       favorite:req.body.product.favorite,
       stars:req.body.product.stars,
-      imageUrl:req.body.product.imageUrl,
+      imageUrl:path[1],
       category:req.body.product.category
     });
     Product.save();
