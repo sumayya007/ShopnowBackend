619902ae85a0614356e0ea893b9632fbf68d0aa2
sumayya <sumayya.kareem6@gmail.com>
2023-11-24 12:45:56 +0530

    added

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -37,7 +37,7 @@ function verifyToken(req, res, next) {
 // });
 
 const Storage=multer.diskStorage({
-  destination:'/images/',
+  destination:'./images',
   filename:function(req,file,cb){
     cb(null,file.originalname)
   }
