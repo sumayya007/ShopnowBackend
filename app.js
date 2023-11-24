1406be28e87580f478e0e131b25234b1999b13f0
sumayya <sumayya.kareem6@gmail.com>
2023-11-24 13:26:17 +0530

    added

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ const Grid=require('gridfs-stream');
 
 const PORT = process.env.PORT || 3000;
 const app = new express();
-// app.use(cors({
-//   origin: 'https://shopnow-bsu7.onrender.com', 
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-//   credentials: true
-// }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://shopnow-bsu7.onrender.com', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  credentials: true
+}));
+// app.use(cors());
 app.use(cookieParser());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
